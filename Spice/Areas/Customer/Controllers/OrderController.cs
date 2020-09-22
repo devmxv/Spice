926497ecb7f27a698dc349d71f33666c4e7e0d4a
devmxv <martin@devmxv.me>
2020-09-22 13:31:10 -0500

    Order History details view created

--- Spice/Areas/Customer/Controllers/OrderController.cs
+++ Spice/Areas/Customer/Controllers/OrderController.cs
@@ -42,5 +42,50 @@ namespace Spice.Areas.Customer
         {
             return View();
         }
+
+        //---Order History
+        [Authorize]
+        public async Task<IActionResult> OrderHistory()
+        {
+            //---Retrive of user id
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //---List of order details view model
+            List<OrderDetailsViewModel> orderList = new List<OrderDetailsViewModel>();
+
+            //---List of orders retrieved
+            List<OrderHeader> OrderHeaderList = await _db.OrderHeader.Include(o => o.ApplicationUser).Where(u => u.UserId == claim.Value).ToListAsync();
+
+            //---for each headers
+            foreach (OrderHeader item in OrderHeaderList)
+            {
+                OrderDetailsViewModel individual = new OrderDetailsViewModel
+                {
+                    OrderHeader = item,
+                    OrderDetails = await _db.OrderDetails.Where(o => o.OrderId == item.Id).ToListAsync()
+                };
+                //---Adding element to list
+                orderList.Add(individual);
+            }
+
+            return View(orderList);
+        }
+
+        //---get - Order details to be used in order history
+        public async Task<IActionResult> GetOrderDetails(int Id)
+        {
+            OrderDetailsViewModel orderDetailsViewModel = new OrderDetailsViewModel()
+            {
+                OrderHeader = await _db.OrderHeader.FirstOrDefaultAsync(m => m.Id == Id),
+                OrderDetails = await _db.OrderDetails.Where(m => m.OrderId == Id).ToListAsync()
+            };
+            orderDetailsViewModel.OrderHeader.ApplicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == orderDetailsViewModel.OrderHeader.UserId);
+            //return partial view
+            return PartialView("_IndividualOrderDetails", orderDetailsViewModel);
+
+        }
     }
+
+    
 }
